1f6d60619f4f56073ce3e5b9e50cfe6a25b7612a
DucLV08 <duc080999@gmail.com>
2023-07-30 13:29:26 +0700

    api register

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -1,13 +1,36 @@
-import express, { Response, Request } from "express";
+import { Response, Request } from "express";
 
 const User = require("../models/user.model");
-const router = express.Router();
 
 async function register(req: Request, res: Response) {
     try {
-        
-        return res.json({})
+        const { email, password, name } = req.body;
+        const alreadyExistsUser = await User.findOne({ where: { email }});
+
+        if(alreadyExistsUser){
+            return res.json({
+                "errorCode": 409,
+                "message": "Email already exists!",
+                "data": null,
+            })
+        }
+
+        const newUser = new User({ email, password, name });
+        const saveUser = await newUser.save(function(error: any){
+            if(error){
+                res.status(500).json({ error: "Cannot register user at the moment!" });
+            }
+        });
+
+        if(saveUser){
+            return res.json({
+                "errorCode": 200,
+                "message": "Thanks for registering",
+                "data": null,
+            })
+        };
     } catch (error) {
+        console.log(error);
         res.status(500).send("Not Found");
     }
 }
